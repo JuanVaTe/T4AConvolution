e4ab45f3ca4f268e117ea8b63ea5bcae60ad3f2d
Keyuan01 <zhaokeyuan72@gmail.com>
2021-10-28 19:17:00 -0500

    Box Blur Keyuan

---
+++ EdgeDetection1.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy
+from PIL import Image
+from scipy import ndimage
+
+def pad_with(vector, pad_width, iaxis, kwargs):
+        pad_value = kwargs.get('padder', 10)
+        vector[:pad_width[0]] = pad_value
+        vector[-pad_width[1]:] = pad_value
+
+def edgeDet1(img):
+    #Se inicializa la imagen
+    Is = Image.open(img)
+    I = Is.convert('L')
+    I = numpy.asarray(I)
+    I = I / 255.0
+
+    I = numpy.pad(I, 30, pad_with, padder=1)
+
+    #kernell Edge Detection version 1
+    k = numpy.array([[1, 0, -1], [0, 0, 0], [-1, 0, 1]])
+
+    #Convolucion
+    J = ndimage.convolve(I, k, mode='constant', cval=0.0)
+
+    #Plot
+    plt.figure(figsize = (15,15))
+
+    #Inserta la imagen original
+    plt.subplot(2,2,1)
+    plt.imshow(Is)
+    plt.xlabel('Imagen original')
+    
+    #Inserta imagen convolucionada
+    plt.subplot(2,2,2)
+    plt.imshow(J)
+    plt.xlabel('Edge Detection 1')
+
+    #Imprime las imagenes
+    plt.grid(False)
+    plt.show()
+
+edgeDet1('Arenoman.jpg')
